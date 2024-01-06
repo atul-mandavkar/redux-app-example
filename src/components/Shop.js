123478558cad1d78a2f0433f3bee1d27e7d5c150
atul-mandackar <atul007atulmandavkar@gmail.com>
2024-01-06 20:23:10 +0530

    Start learning of using redux library in js Redux (boilerplate code) is used as alternative for context api, redux help us to manage state properly like ( context api).
    First to install redux in app , you need redux, react-redux and middleware redux-thunk, so to install from cmd we write
    npm i redux react-redux redux-thunk
    After installing check in package.json file, whether installed or not.
    
    After that we removed unneccesary files from our react app and added bootstrap css and js file for unique styling.
    then we added Navbar component from bootstrap
    Then we created Shop component, which have two buttons for adding and substracting the amount.
    Then we displayed those both component in App.js
    
    Then we created a folder named state, which contain the state.
    In that folder two separate folder for action-creator and reducers.
    In action-creator folder we make actions inside index.js file (here actions are deposit and withdraw).
    In reducers folder we created a reducer function which actually make changes in state value.

---
+++ src/components/Shop.js
@@ -0,0 +1,17 @@
+import React from "react";
+// Shop component is created 
+
+function Shop() {
+  return (
+    <div>
+      <div className="container">
+        <h2>Deposit / Withdraw money</h2>
+        <button className="btn btn-primary mx-2">-</button>
+        Balance update
+        <button className="btn btn-primary mx-2">+</button>
+      </div>
+    </div>
+  );
+}
+
+export default Shop;
---
+++ src/state/action-creators/index.js
@@ -0,0 +1,20 @@
+// First seperate folder is created for action creator (action creator means methods who order the reducer to make action and chang state value )
+// actions are created in file index.js and here actions are deposite and withdraw
+
+export const depositMoney = (amount) => { // deposite action
+    return (dispatch) => {
+        dispatch({
+            type: 'deposite',
+            payload: amount
+        })
+    }
+}
+
+export const WithdrawMoney = (amount) => { // withdraw action
+    return (dispatch) => {
+        dispatch({
+            type: 'withdraw',
+            payload: amount
+        })
+    }
+}
